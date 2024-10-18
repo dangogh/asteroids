2141f1cf51320b4a88ea5bdaad74a3833abdcdd1
Dan Kirkwood <dangogh@gmail.com>
2024-10-17 17:48:38 -0700

    Add game loop

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +17,7 @@ def main():
 
         screen.fill(color=(0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
